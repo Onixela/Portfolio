e8c1db4ab9ea8c7c8d1e8566a340765a5fa47698
Aleksandr Lazarev <aleksandrlazarev@MacBook-Air-Aleksandr.local>
2022-03-08 22:29:50 +0200

    descriptive message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontSize: {
+        '5xl': '3rem',
+        '6xl': '4rem',
+        '7xl': '5rem',
+
+},
+      fontFamily: {
+
+        'righteous':['"Righteous"'],
+        'poppins':['"Poppins"'],
+      }
+
+    },
   },
   plugins: [],
 }
+
